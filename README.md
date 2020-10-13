0688ec0481e6be92f43cee577a8d60ef59b3e22b
Maciej Zygadło <Xsardas665@gmail.com>
2020-10-13 21:18:12 +0200

    Readme chancges, beginning of handlings turn

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # TicTacToe PROJECT
-
-Current Working On : Board
--- main.py
+++ main.py
@@ -6,5 +6,15 @@ def display_board():
     print(board[0] + "|" + board[1] + "|" + board[2])
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
+def play_game():
+    # display initial Board
+    display_board()
+    handle_turn()
 
-display_board()
+def handle_turn():
+    position = input("Choose position [1-9] :")
+    position = int(position) - 1
+    board[position] = "X"
+    display_board()
+
+play_game()
